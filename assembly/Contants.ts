739f28fde0be13770c23351db94cf4884ca15191
eder lobo <ederandreslobo1998@gmail.com>
2023-09-06 10:25:54 -0500

    updating hash of bytecode

--- assembly/Contants.ts
+++ assembly/Contants.ts
@@ -2,5 +2,5 @@ import { Base58 } from "@koinos/sdk-as";
 
 export namespace Constants {
   export const GOVERNANCE_CONTRACT_ID = Base58.decode('');
-  export const HASH_BYTECODE: u8[] = [ 18,32,9,198,132,177,155,6,117,46,67,15,167,238,106,192,107,211,238,73,50,220,96,140,176,214,8,186,66,69,230,165,84,232 ];
+  export const HASH_BYTECODE: u8[] = [ 18,32,163,202,113,173,92,168,1,8,2,101,174,236,120,66,161,213,28,29,34,14,100,97,199,42,152,52,79,73,184,229,165,50 ];
 }
